96ff840f1970497381cda58eedac72ec31961e2a
Nuttanee Knight <nuttaneeknight@gmail.com>
2022-10-11 18:25:52 -0400

    updating the script for the json server to port 5000 for api call

--- src/Layout/Home/Home.js
+++ src/Layout/Home/Home.js
@@ -6,16 +6,18 @@ import { listDecks, updateDeck } from "../../utils/api";
 function Home ({ updateDecks, deckLength }) {
     const [decks, setDecks] = useState([])
 
+    //fetch the deck from api
     useEffect(() =>{
-        //in case the user click the wrong button abort the API call, good practice
         const abortController = new AbortController()
         //fetching listDecks from the API server by using async await
         const fetchDecks = async () => {
             const ApiDecks = await listDecks(abortController.signal)
             //change the state 'setDecks' to listDecks
-            setDecks(() => ApiDecks)
+            setDecks(ApiDecks)
+            console.log(2)
         }
         // call fetchDecks
+        console.log(1)
         fetchDecks()
         return () => abortController.abort()
     }, [deckLength])
@@ -25,9 +27,10 @@ function Home ({ updateDecks, deckLength }) {
         <div>
             <div className='row mx-auto'>
                 <Link to={'/decks/new'} className='btn btn-secondary mb-2'>
-                <button type="button" class="btn btn-secondary">+ Create Deck</button>   
+                <button type="button" className="btn btn-secondary">+ Create Deck</button>   
                 </Link>
             </div>
+            {/* display the existing deck */}
             <div className='row mx-auto'>
                 {decks.map((deck) => 
                 <DeckList 
