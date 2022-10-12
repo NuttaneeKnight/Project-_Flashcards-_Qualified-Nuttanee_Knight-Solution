a65f0d7001fa625e4b75accdc51388ba17b8a3f9
Nuttanee Knight <nuttaneeknight@gmail.com>
2022-10-11 22:03:16 -0400

    Up dated some styling to the icons and some alignments

--- src/Layout/Decks/Deck.js
+++ src/Layout/Decks/Deck.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams, useHistory, useRouteMatch } from "react-router-dom";
-import { deleteCard, readDeck } from "../../utils/api/index";
-import { deleteDeck } from "../../utils/api/index";
+import { deleteCard, readDeck } from "../../utils/api/index.js";
+import { deleteDeck } from "../../utils/api/index.js";
 
-//create a function retrieves an updated deck's id, fetch it's card data and set the deck useState to have content
-const Deck = ({ updateDecks }) => {
+// create a function that retrieves an updated deck's id,
+// fetches it's card data, and sets the deck's useState to
+// contain it's content
+function Deck({ updateDecks }) {
   const [deck, setDeck] = useState([]);
   const { deckId } = useParams();
   const history = useHistory();
@@ -12,7 +14,8 @@ const Deck = ({ updateDecks }) => {
   const { id, name, description, cards } = deck;
 
   useEffect(() => {
-    const abortController = new AbortController();
+    // const abortController = window.AbortController;
+    const abortController = new AbortController()
     const deckInfo = async () => {
       const response = await readDeck(deckId, abortController.signal);
       setDeck(() => response);
@@ -20,79 +23,121 @@ const Deck = ({ updateDecks }) => {
     deckInfo();
     return () => abortController.abort();
   }, [deckId]);
-  //create a delete handler with prompt window pop up, if confirmed delete it
+
+  // create a handler for the delete button
   const deleteHandler = async () => {
+    // if the button is clicked and confirmed by the user, delete the deck using it's id
     if (
-      window.confirm("Delete this deck? You will not be able to revocer it.")
+      window.confirm(
+        "Are you sure you want to delete this deck? You will not be able to recover it."
+      )
     ) {
       await deleteDeck(id);
-      //utilize the updateDecks() to remove it from the deck
+      // use updateDecks() to subtract it from the card deck
       updateDecks(-1);
-      //go to home page
+      // redirect to the home page
       history.push("/");
+      // if the delete is not confirmed, leave the deck as is and remain on the same page
     } else {
-      //ramains on the same page
       history.go(0);
     }
   };
-  // if no deck or card, return 'loading...'
+
+  // if there is no deck or no cards, return the following webpage
+  // that displays "loading..."
   if (!deck || !cards) {
     return (
       <div className="spinner-border text-primary" role="status">
         <span className="sr-only">Loading...</span>
       </div>
+      // if a deck or cards are present, return a webpage with the following content
     );
-    //if it's present return content
   } else {
     return (
       <div className="col-9 mx-auto">
+        {/* a navigation bar with the following links */}
         <nav aria-label="breadcrumb">
-          <ol className="breadcrumbs">
+          <ol className="breadcrumb">
+            {/* a link to the home page */}
             <li className="breadcrumb-item">
-              <Link to={"/"}>Home</Link>
+              <Link to={"/"}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 20 20">
+                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+              </svg>
+                Home
+              </Link>
             </li>
+
+            {/* a string containing the deck's name */}
             <li className="breadcrumb-item">{name}</li>
           </ol>
         </nav>
 
-        {/* deck container with name, description, btn study, btn edit, btn add cards, btn delete */}
+        {/* a container holding the card deck, including their name,
+                description, a button to study it, a button to edit it, a 
+                button to add cards to the deck, and a button to delete it */}
+
         <div className="card border-0 mb-4">
           <div className="card-body">
+            {/* deck name */}
             <div className="row px-3">
               <h5 className="card-title">{name}</h5>
             </div>
 
+            {/* deck description */}
             <p className="card-text">{description}</p>
 
-            <div className="row px-3"></div>
-            <Link to={`/decks/${id}edit`} className="btn btn-secondary">
-              Edit
-            </Link>
+            <div className="row px-3">
+              {/* edit button */}
+              <Link to={`/decks/${id}/edit`} className="btn btn-secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 20 20">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+              </svg>
+                Edit
+              </Link>
 
-            <Link to={`/decks/${id}/study`} className="btn btn-primary ml-2">
-              Study
-            </Link>
+              {/* study button */}
+              <Link to={`/decks/${id}/study`} className="btn btn-primary ml-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-bookmark" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M6 8V1h1v6.117L8.743 6.07a.5.5 0 0 1 .514 0L11 7.117V1h1v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8z"/>
+                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+              </svg>
+                Study
+              </Link>
 
-            <Link
-              to={`/decks/${id}/cards/new`}
-              className="btn btn-primary ml-2"
-            >
-              + Add Cards
-            </Link>
+              {/* add cards button */}
+              <Link
+                to={`/decks/${id}/cards/new`}
+                className="btn btn-primary ml-2"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 1 20 16">
+                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                </svg>
+                Add Cards
+              </Link>
 
-            <button
-              onClick={deleteHandler}
-              name="delete"
-              // controled input
-              value={id}
-              className="btn btn-danger ml-auto"
-            >
-              {/* trash icon */}
-              <i className="fa fa-trash" aria-hidden="true"></i>
-            </button>
+              {/* delete button */}
+              <button
+                onClick={deleteHandler}
+                name="delete"
+                value={id}
+                className="btn btn-danger ml-auto"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+                {/* Delete */}
+                <i className="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </div>
           </div>
         </div>
-        {/* //   cards container for all cards, front, back, edit bth , delete btn */}
+
+        {/* a container containing all cards, including their front, back
+                an edit button, and a delete button */}
+
         <div className="row pl-3 pb-2">
           <h1>Cards</h1>
         </div>
@@ -102,21 +147,30 @@ const Deck = ({ updateDecks }) => {
             <div className="col">
               <div className="card">
                 <div className="row card-body">
+                  {/* front */}
                   <p className="col-6 card-text">{card.front}</p>
+
+                  {/* back */}
                   <p className="col-6 card-text">{card.back}</p>
                 </div>
+
                 <div className="d-flex justify-content-end p-2">
+                  {/* edit button */}
                   <Link
                     to={`${url}/cards/${card.id}/edit`}
                     className="btn btn-secondary"
                   >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 20 20">
+                      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                    </svg>
                     Edit
                   </Link>
+
                   <button
                     onClick={async () => {
                       if (
                         window.confirm(
-                          "Delete this deck? You will not be able to revocer it."
+                          "Are you sure you want to delete this card? You will not be able to recover it."
                         )
                       ) {
                         await deleteCard(card.id);
@@ -126,11 +180,16 @@ const Deck = ({ updateDecks }) => {
                         history.go(0);
                       }
                     }}
-                    name='deleteCard'
+                    name="deleteCard"
                     value={card.id}
-                    className='btn btn-danger ml-2'
+                    className="btn btn-danger ml-2"
                   >
-                    <i className='fa fa-trash' aria-hidden='true'></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                      <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg>
+                    {/* Delete */}
+                    <i className="fa fa-trash" aria-hidden="true"></i>
                   </button>
                 </div>
               </div>
@@ -140,6 +199,7 @@ const Deck = ({ updateDecks }) => {
       </div>
     );
   }
-};
+}
 
 export default Deck;
+
